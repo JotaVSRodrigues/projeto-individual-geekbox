f5902b14af8ff780642e75b099e2fce2a1487414
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-07-11 12:10:38 -0300

    home script code to load items

--- src/controllers/estatisticaController.js
+++ src/controllers/estatisticaController.js
@@ -10,7 +10,6 @@ var estatisticaModel = require("../models/estatisticaModel");
 function consumoMensal(req, res) {
     var usuarioId = req.params.id; 
     
-    
     estatisticaModel.consumoMensal(usuarioId)
         .then(function(resultado) {
             res.status(200).json(resultado);
